97a369fb4e365b7ec5eb5b054596f58fbe3f5ac1
SL0TR <mohaiminislam94@gmail.com>
2018-05-24 22:13:16 +0600

    initialize repo and replace bpwn startup content

---
+++ src/layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="transparent"
+        class="text-black"
+      >
+        <img class="bpwn-logo" src="../statics/bpwn-logo.png" alt="BPWN Logo">
+        <q-toolbar-title>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <!-- <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+    >
+      <q-list
+      >
+        <q-list-header>Essential Links</q-list-header>
+        <q-item @click.native="openURL('http://quasar-framework.org')">
+          <q-item-side icon="school" />
+          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+        </q-item>
+        <q-item @click.native="openURL('https://github.com/quasarframework/')">
+          <q-item-side icon="code" />
+          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
+        </q-item>
+        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
+          <q-item-side icon="chat" />
+          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
+        </q-item>
+        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
+          <q-item-side icon="record_voice_over" />
+          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
+        </q-item>
+        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
+          <q-item-side icon="rss feed" />
+          <q-item-main label="Twitter" sublabel="@quasarframework" />
+        </q-item>
+      </q-list>
+    </q-layout-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'PageLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+  /* GLOBAL STYLES */
+  .bpwn-logo {
+    height: 50px;
+    width: 140px;
+  }
+</style>
