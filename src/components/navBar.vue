1d86a6c3f779323f18d08a833e2a99f0254aac27
SL0TR <mohaiminislam94@gmail.com>
2018-05-26 19:06:22 +0600

    make association with iawp page 1.0

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -50,6 +50,33 @@
           </div>
         </div>
       </q-btn-dropdown>
+      <q-btn-dropdown class="text-primary" label="Int. Affiliation" >
+        <div class="row">
+          <div class="col-12 text-center">
+            <q-btn class="text-primary full-width" label="Association with IAWP" flat @click="$router.push('/int-affiliation/association-with-iawp')"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <q-btn class="text-primary full-width" label="1st Asia Conference" flat @click="$router.push('/int-affiliation/first-asia-conference')"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <q-btn class="text-primary full-width" label="Networking With Other Org." flat @click="$router.push('/int-affiliation/networking-with-other-org')"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+          <q-btn class="text-primary full-width" label="Higher Study &amp; Fellowhsip" flat @click="$router.push('/int-affiliation/higher-study-and-fellowship')"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+          <q-btn class="text-primary full-width" label="Achievements" flat @click="$router.push('/int-affiliation/achievements')"/>
+          </div>
+        </div>
+      </q-btn-dropdown>
       <q-btn class=" nav-btn" label="UN MISSION" flat @click="$router.push('/un-mission')"/>
       <q-btn class=" nav-btn" label="Testimonials" flat @click="$router.push('/testimonials')"/>
       <q-btn class=" nav-btn" label="gallery" flat @click="$router.push('/gallery')"/>
---
+++ src/pages/int-affiliation/higher-study-and-fellowship.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col-xs-10 offset-xs-1  q-mt-xl">
+      <div class="text-center q-display-2 text-primary text-bold featured-header uppercase">Vision &amp; Formation</div>
+    </div>
+    <div class="col-lg-4 offset-lg-4 col-xs-8 offset-xs-2 q-mt-sm q-mb-xl">
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
